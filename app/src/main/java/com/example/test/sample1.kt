08e2fc54708a133f492e281b6f02a963fb51340d
이영주 <yungju9982@naver.com>
2022-05-27 20:31:48 +0900

    val vs var

--- app/src/main/java/com/example/test/sample1.kt
+++ app/src/main/java/com/example/test/sample1.kt
@@ -7,7 +7,6 @@ fun main(){
 
 
 //1. 함수
-
 fun helloWorld() { // Unit = void return type 없어도 생략 가능
     println("Hello World! \n");
 }
@@ -16,3 +15,24 @@ fun helloWorld() { // Unit = void return type 없어도 생략 가능
 fun add(a : Int, b: Int) : Int {
     return a+b
 }
+
+//2. val vs var
+//val = value (변하지 않는 값)
+//var = variable (변할 수 있는 값)
+fun hi(){
+    val a: Int = 10
+    // a = 20 //(오류 : 값 변경 불가능)
+    var b: Int = 20
+    b = 200 // 값 변경 가능
+
+    // 초기화 해줄 경우 타입 생략 가능
+    val c = 100
+    var d = 200
+    var name = "Yeongju"
+
+    // 초기화하지 않는 경우 타입 생략 불가능
+    //var e (오류)
+    var e : String
+    var f : Int
+}
+
