dfb9708c693287c332aa738bb547d45c71377e7e
이영주 <yungju9982@naver.com>
2022-05-27 20:53:43 +0900

    String Template

--- app/src/main/java/com/example/test/sample1.kt
+++ app/src/main/java/com/example/test/sample1.kt
@@ -2,7 +2,17 @@ package com.example.test
 
 fun main(){
     helloWorld()
-    println(add(2, 3))
+    add(2, 4)
+
+    // 3. String Template
+    val firstName = "Julie"
+    val lastName = "Lee"
+    println("I'm ${firstName + lastName} I'm 24") //${변수명 + 변수명 + ...}
+    println("I'm ${firstName} ${lastName}") // 따로따로도 가능
+    println("I wanna use \$dollar") // \$ = $표시만
+    println("Is this true? ${0==1}")
+
+    
 }
 
 
